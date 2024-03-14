10edc9536c005519f099ed5b89d98b10148f8adb
anton <anton.sapozhnik@gmail.com>
2024-03-14 15:04:51 +0200

    Добавив список вибору завдання на 1-му екрані

--- app/src/main/java/ua/edu/lntu/ipz_cw_4_sapozhnik_anton/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/ipz_cw_4_sapozhnik_anton/MainActivity.kt
@@ -3,9 +3,12 @@ package ua.edu.lntu.ipz_cw_4_sapozhnik_anton
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -14,6 +17,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -52,6 +56,19 @@ fun Screen1(items: List<Pair<String, String>>, onItemClick: (String) -> Unit) {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun ListItem(taskId: String, onItemClick: (String) -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onItemClick(taskId) }
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = taskId)
     }
 }
 
