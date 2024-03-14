4a710f6b7947626fa45b9ee7c14eb410e381f10c
anton <anton.sapozhnik@gmail.com>
2024-03-14 15:15:09 +0200

    функція навігації екрану 2 (додав 2 екран)

--- app/src/main/java/ua/edu/lntu/ipz_cw_4_sapozhnik_anton/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/ipz_cw_4_sapozhnik_anton/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,10 +14,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -41,7 +47,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Screen1(items: List<Pair<String, String>>, onItemClick: (String) -> Unit) {
+fun Screen1(items: List<Pair<String, String>>, onItemClick: (String) -> Unit) {//виправленно
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -69,6 +75,29 @@ fun ListItem(taskId: String, onItemClick: (String) -> Unit) {
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(text = taskId)
+    }
+}
+
+@Composable
+fun Screen2(taskText: String?, onDoneClick: () -> Unit) {
+    var isTaskCompleted by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Екран 2")
+        Spacer(modifier = Modifier.height(16.dp))
+        taskText?.let { // Перевіряємо, чи є текст завдання, перед тим як відображати
+            Text(it)
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+
     }
 }
 
