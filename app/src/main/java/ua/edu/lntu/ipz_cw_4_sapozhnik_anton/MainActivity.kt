5543f14f80e49d7f40633dadf66a16ee423ee2aa
anton <anton.sapozhnik@gmail.com>
2024-03-14 15:20:57 +0200

    додав функцію App для відображення  вмісту додатка і управління екранами та в майбутньому вмістом.

--- app/src/main/java/ua/edu/lntu/ipz_cw_4_sapozhnik_anton/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/ipz_cw_4_sapozhnik_anton/MainActivity.kt
@@ -1,5 +1,6 @@
 package ua.edu.lntu.ipz_cw_4_sapozhnik_anton
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,12 +15,20 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -39,7 +48,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    App()
                 }
             }
         }
@@ -107,11 +116,12 @@ fun Screen2(taskText: String?, onDoneClick: () -> Unit) {
         }
     }
 }
-
-@Preview(showBackground = true)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-    IPZ_CW_4_Sapozhnik_AntonTheme {
-        Greeting("Android")
+fun App() {
+    var selectedItem by remember { mutableStateOf<String?>(null) }
+    var currentScreen by remember { mutableIntStateOf(0) }
+
     }
-}
+
